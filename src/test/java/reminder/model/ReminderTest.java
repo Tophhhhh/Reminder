7503cee422a746b0bce03e86feb2e556a1bce5cb
Tophhhhh <96735128+Tophhhhh@users.noreply.github.com>
2022-08-07 16:34:53 +0200

    Update ReminderTest.java

--- src/test/java/reminder/model/ReminderTest.java
+++ src/test/java/reminder/model/ReminderTest.java
@@ -11,20 +11,20 @@ public class ReminderTest {
 	
 	@Test
 	public void testSetter() {
-		// 
+		// G I V E N
 		
 		// W H E N
 		
-		//
+		// T H E N
 	}
 	
 	@Test
 	public void testGetter() {
-		// 
+		// G I V E N
 		
 		// W H E N
 		
-		//
+		// T H E N
 	}
 
 }
